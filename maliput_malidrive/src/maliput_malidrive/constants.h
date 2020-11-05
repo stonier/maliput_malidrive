6d8a28bd9ac299a082d29880a6360a33f39bd621
Franco Cipollone <53065142+francocipollone@users.noreply.github.com>
2020-11-05 15:31:06 -0300

    Splitts malidrive/constants.h (#684)
    * Moves constants.h to maliput_malidrive.
    
    * Install file.
    
    * split constants.
    
    * Adapt all files.
    
    * Addresses comments.

---
+++ maliput_malidrive/src/maliput_malidrive/constants.h
@@ -0,0 +1,23 @@
+// Copyright 2020 Toyota Research Institute
+#pragma once
+
+namespace malidrive {
+namespace constants {
+
+/// maliput::api::RoadGeometry properties.
+/// Empirically good enough configurations based on internal ODRM settings and
+/// available testing geometries.
+static constexpr double kLinearTolerance{5e-2};   // [m]
+static constexpr double kAngularTolerance{1e-2};  // [rad]
+static constexpr double kScaleLength{1.};         // [m]
+/// Stricter tolerances.
+static constexpr double kStrictLinearTolerance{1e-12};   // [m]
+static constexpr double kStrictAngularTolerance{1e-12};  // [rad]
+
+/// Builder - SpeedLimitRule arbitrary defaults.
+static constexpr double kSpeedTolerance{1e-4};             // [m/s]
+static constexpr double kDefaultMinSpeedLimit{0.};         // [m/s]
+static constexpr double kDefaultMaxSpeedLimit{40. / 3.6};  // [m/s] --> 40km/h
+
+}  // namespace constants
+}  // namespace malidrive
