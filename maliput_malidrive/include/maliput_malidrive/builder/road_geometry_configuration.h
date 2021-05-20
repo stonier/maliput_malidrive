c38fd5e5aa29adb156fc134f05e5a3b9b2888e0c
Chien-Liang Fok <liang.fok@tri.global>
2021-05-20 15:07:59 -0400

    Polish road_geometry_configuration.h (#63)
    Increase compliance with the Google Style Guide style guide and
    docstring clarity.

--- maliput_malidrive/include/maliput_malidrive/builder/road_geometry_configuration.h
+++ maliput_malidrive/include/maliput_malidrive/builder/road_geometry_configuration.h
@@ -1,6 +1,9 @@
 // Copyright 2019 Toyota Research Institute
 #pragma once
 
+#include <optional>
+#include <string>
+
 #include "maliput/api/road_network.h"
 #include "maliput/math/vector.h"
 
@@ -10,16 +13,17 @@
 namespace malidrive {
 namespace builder {
 
-/// Holds the policy for concurrency optimization
-/// during lane creation.
+/// Policy for use of concurrency while building a RoadGeometry.
 struct BuildPolicy {
   enum Type {
     kSequential = 0,
     kParallel,
   };
 
-  /// Convert string to a Type.
-  /// @param type String to be translated to a Type.
+  /// Converts string to a Type.
+  ///
+  /// @param type String to be translated to a Type. Valid strings match the enumerator's name except without the
+  /// leading 'k' and is all lower case.
   /// @returns A BuildPolicy::Type value
   /// @throws maliput::common::assertion_error When `type` isn't a valid Type.
   static BuildPolicy::Type FromStrToType(const std::string& type);
@@ -28,43 +32,48 @@ struct BuildPolicy {
   std::optional<int> num_threads{};
 };
 
-/// Structure to wrap RoadGeometry parameters.
+/// RoadGeometry construction parameters.
 struct RoadGeometryConfiguration {
-  /// Determines the flexibility to follow the OpenDrive standard when building the road geometry.
+  /// Level of flexibility in terms of adhering to the OpenDrive standard when constructing a RoadGeometry. This is
+  /// useful when working with .xodr files that violate the OpenDrive standard.
   enum class StandardStrictnessPolicy {
-    kPermissive,  ///< Allows to the road geometry to not completely meet the standard.
-    kStrict,      ///< No flexibility at all.
+    kPermissive,  ///< Allow the .xodr file to violate the standard. Continue on best-effort-basis.
+    kStrict,      ///< Do not allow violation of the standard.
   };
 
-  /// Determines whether a simplification to the geometry should be applied or
-  /// not.
+  /// Degree to which the RoadGeometry should be simplified.
   enum class SimplificationPolicy {
     kNone,                                         ///< No simplification must be done.
     kSimplifyWithinToleranceAndKeepGeometryModel,  ///< Merge geometry pieces and keep the original geometry model.
   };
 
-  /// Determines whether the tolerance and scale length should be automatically
-  /// computed or not.
+  /// How the tolerance and scale length should be computed.
   enum class ToleranceSelectionPolicy {
     kManualSelection,     ///< Manual adjustment.
     kAutomaticSelection,  ///< Automatic selection.
   };
 
-  /// Convert a string to a SimplificationPolicy.
-  /// @param policy String to be translated to a SimplificationPolicy.
-  /// @returns A SimplificationPolicy value
+  /// Converts a string to a SimplificationPolicy.
+  ///
+  /// @param policy String to be translated to a SimplificationPolicy. Valid strings match the enumerator's name except
+  /// without the leading 'k' and is all lower case.
+  /// @returns A SimplificationPolicy value.
   /// @throws maliput::common::assertion_error When `policy` isn't a valid SimplificationPolicy.
   static SimplificationPolicy FromStrToSimplificationPolicy(const std::string& policy);
 
-  /// Convert a string to a ToleranceSelectionPolicy.
-  /// @param policy String to be translated to a ToleranceSelectionPolicy.
-  /// @returns A ToleranceSelectionPolicy value
+  /// Converts a string to a ToleranceSelectionPolicy.
+  ///
+  /// @param policy String to be translated to a ToleranceSelectionPolicy. Valid strings match the enumerator's name
+  /// except without the leading 'k' and is all lower case.
+  /// @returns A ToleranceSelectionPolicy value.
   /// @throws maliput::common::assertion_error When `policy` isn't a valid ToleranceSelectionPolicy.
   static ToleranceSelectionPolicy FromStrToToleranceSelectionPolicy(const std::string& policy);
 
-  /// Convert a string to a StandardStrictnessPolicy.
-  /// @param policy String to be translated to a StandardStrictnessPolicy.
-  /// @returns A StandardStrictnessPolicy value
+  /// Converts a string to a StandardStrictnessPolicy.
+  ///
+  /// @param policy String to be translated to a StandardStrictnessPolicy. Valid strings match the enumerator's name
+  /// except without the leading 'k' and is all lower case.
+  /// @returns A StandardStrictnessPolicy value.
   /// @throws maliput::common::assertion_error When `policy` isn't a valid StandardStrictnessPolicy.
   static StandardStrictnessPolicy FromStrToStandardStrictnessPolicy(const std::string& policy);
 
