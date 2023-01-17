29468dd28f6065aa6aa1bff9fecf445fc7f8e56d
Franco Cipollone <franco.c@ekumenlabs.com>
2023-01-17 11:01:32 -0300

    Matches changes in maliput.
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>

--- src/maliput_malidrive/builder/road_geometry_builder.cc
+++ src/maliput_malidrive/builder/road_geometry_builder.cc
@@ -559,8 +559,10 @@ std::unique_ptr<const maliput::api::RoadGeometry> RoadGeometryBuilder::DoBuild()
     rg->AddBranchPoint(std::move(bps_[i]));
   }
 
-  maliput::log()->trace("Using kdtree for spacial reorganization.");
-  rg->InitializeStrategy<maliput::geometry_base::KDTreeStrategy>();
+  maliput::log()->trace(
+      "Initializing maliput::geometry_base::KDTreeStrategy for improving RoadGeometry::ToRoadPosition and "
+      "RoadGeometry::FindRoadPosition methods.");
+  rg->InitializeStrategy<maliput::geometry_base::KDTreeStrategy>(0.1);
   maliput::log()->trace("RoadGeometry is built.");
   return rg;
 }
